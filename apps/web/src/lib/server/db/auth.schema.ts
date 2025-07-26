bc6299dc5c9238d760bd1686056932bf9861e55e
Aubin DETERNE <aubind97@orange.fr>
2025-07-26 15:24:32 +0200

    Update better-auth, Add Organization schema and basic Frame schema

--- apps/web/src/lib/server/db/auth.schema.ts
+++ apps/web/src/lib/server/db/auth.schema.ts
@@ -27,6 +27,7 @@ export const session = sqliteTable("session", {
 	userId: text("user_id")
 		.notNull()
 		.references(() => user.id, { onDelete: "cascade" }),
+	activeOrganizationId: text("active_organization_id"),
 });
 
 export const account = sqliteTable("account", {
@@ -58,4 +59,39 @@ export const verification = sqliteTable("verification", {
 	expiresAt: integer("expires_at", { mode: "timestamp" }).notNull(),
 	createdAt: integer("created_at", { mode: "timestamp" }).$defaultFn(() => /* @__PURE__ */ new Date()),
 	updatedAt: integer("updated_at", { mode: "timestamp" }).$defaultFn(() => /* @__PURE__ */ new Date()),
+});
+
+export const organization = sqliteTable("organization", {
+	id: text("id").primaryKey(),
+	name: text("name").notNull(),
+	slug: text("slug").unique(),
+	logo: text("logo"),
+	createdAt: integer("created_at", { mode: "timestamp" }).notNull(),
+	metadata: text("metadata"),
+});
+
+export const member = sqliteTable("member", {
+	id: text("id").primaryKey(),
+	organizationId: text("organization_id")
+		.notNull()
+		.references(() => organization.id, { onDelete: "cascade" }),
+	userId: text("user_id")
+		.notNull()
+		.references(() => user.id, { onDelete: "cascade" }),
+	role: text("role").default("member").notNull(),
+	createdAt: integer("created_at", { mode: "timestamp" }).notNull(),
+});
+
+export const invitation = sqliteTable("invitation", {
+	id: text("id").primaryKey(),
+	organizationId: text("organization_id")
+		.notNull()
+		.references(() => organization.id, { onDelete: "cascade" }),
+	email: text("email").notNull(),
+	role: text("role"),
+	status: text("status").default("pending").notNull(),
+	expiresAt: integer("expires_at", { mode: "timestamp" }).notNull(),
+	inviterId: text("inviter_id")
+		.notNull()
+		.references(() => user.id, { onDelete: "cascade" }),
 });
---
+++ apps/web/src/lib/server/db/frame.schema.ts
@@ -0,0 +1,17 @@
+import { integer, sqliteTable, text } from "drizzle-orm/sqlite-core";
+import { organization } from "./auth.schema.ts";
+
+export const frame = sqliteTable("frame", {
+	id: text("id").primaryKey(),
+	organizationId: text("organization_id")
+		.notNull()
+		.references(() => organization.id),
+	name: text('name').notNull(),
+	apiUrl: text('apiUrl').notNull(),
+	createdAt: integer("created_at", { mode: "timestamp" })
+		.$defaultFn(() => /* @__PURE__ */ new Date())
+		.notNull(),
+	updatedAt: integer("updated_at", { mode: "timestamp" })
+		.$defaultFn(() => /* @__PURE__ */ new Date())
+		.notNull(),
+});
