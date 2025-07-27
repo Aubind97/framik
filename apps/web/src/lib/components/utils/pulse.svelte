6a6b9030e5a940e84cf30d3aafbfe5ffe4ed415a
Aubin DETERNE <aubind97@orange.fr>
2025-07-27 17:57:09 +0200

    Define primary colors

--- apps/web/src/lib/components/utils/pulse.svelte
+++ apps/web/src/lib/components/utils/pulse.svelte
@@ -3,6 +3,6 @@
 </script>
 
 <span class="relative flex size-3">
-    <span class="absolute inline-flex h-full w-full rounded-full opacity-75 {enabled ? 'bg-sky-400 animate-ping' : ''}"></span>
-    <span class="relative inline-flex size-3 rounded-full {enabled ? 'bg-sky-500' : 'bg-muted'}"></span>
+    <span class="absolute inline-flex h-full w-full rounded-full opacity-75 {enabled ? 'bg-primary-foreground animate-ping' : ''}"></span>
+    <span class="relative inline-flex size-3 rounded-full {enabled ? 'bg-primary' : 'bg-muted'}"></span>
 </span>
