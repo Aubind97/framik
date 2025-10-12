4a662b5a81e2bc40614576c22e4f37f34cb70d0b
Aubin DETERNE <aubind97@orange.fr>
2025-10-12 14:35:31 +0200

    FIX deprecated slot

--- apps/web/src/lib/components/widgets/core/widget-frame.svelte
+++ apps/web/src/lib/components/widgets/core/widget-frame.svelte
@@ -1,3 +1,9 @@
+<script lang="ts">
+import { type Snippet } from "svelte";
+
+let { children }: { children?: Snippet } = $props();
+</script>
+
 <div class="border p-8 lg:p-16 rounded h-full">
-    <slot />
+    {@render children?.()}
 </div>
